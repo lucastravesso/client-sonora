b83e0bcb9a446ebd51d95d19ff803d6e66c6ef72
lucastravesso <lucasescobar.ti@hotmail.com>
2022-03-20 15:19:42 -0300

    alteração pagina de pedido

--- src/pages/OrderSelectedPage/index.jsx
+++ src/pages/OrderSelectedPage/index.jsx
@@ -13,20 +13,40 @@ export default function OrderSelectedPage() {
 
     const [order, setOrder] = useState([]);
     const [products, setProducts] = useState([]);
+    const [cartProducts, setCartProducts] = useState([]);
 
     const history = useHistory();
 
-    async function getOrder(){
+    async function getProducts() {
+        try {
+            await api.get(`/order/prod/${localStorage.getItem('order-id')}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            }).then(resProd => {
+                setProducts(resProd.data)
+                setCartProducts(resProd.data.cartProducts)
+            })
+
+        } catch (err) {
+            alert("Nao foi possivel buscar produtos")
+        }
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, []);
+
+    async function getOrder() {
 
         try {
-            
+
             await api.get(`/order/${localStorage.getItem('order-id')}`, {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem("accessToken")}`
                 }
             }).then(res => {
                 setOrder(res.data)
-                setProducts(res.data.products)
             })
 
         } catch (err) {
@@ -34,7 +54,9 @@ export default function OrderSelectedPage() {
         }
     }
 
-    useEffect(()=>{getOrder()}, [])
+    useEffect(() => { getOrder() }, [])
+
+    console.log(cartProducts)
 
     return (
         <>
@@ -42,25 +64,34 @@ export default function OrderSelectedPage() {
             <div className="container-order-selected">
                 <div className="selected-left">
 
-                    <table>
-                        <thead>
-                                <tr>
-                                    <td>{'ID do Pedido ' +  order.id}</td>
-                                    <td>{'Status do Pedido ' + order.status}</td>
-                                    <td>{'Data do Pedido  ' + order.orderDate}</td>
-                                </tr>
-                        </thead>
-                        <tbody>
-                            {products.map(p =>(
-                                <tr key={p.id}>
-                                    <td>{p.prod_name}</td>
-                                    <td>{p.prod_price}</td>
-                                    <td>{p.prod_spec}</td>
-                                </tr>
-                            ))}
-                        </tbody>
+                    <table className="top-table">
+                        <tr>
+                            <td>{'Pedido : ' + order.id}</td>
+                            <td >{'Status do Pedido : ' + order.status}</td>
+                            <td>{'Data do Pedido : ' + order.orderDate}</td>
+                            <td colSpan={2}>{'Preço total do pedido : ' + Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(products.totalPrice)}</td>
+                        </tr>
                     </table>
-
+                    <table className="middle-table">
+                        <tr className="bar">
+                            <td>Produto</td>
+                            <td>Preço Unitario</td>
+                            <td>Especificação</td>
+                            <td>Fabricante</td>
+                            <td>Quantidade total</td>
+                            <td>Preço Total</td>
+                        </tr>
+                        {cartProducts.map(p => (
+                            <tr>
+                                <td>{p.productDTO.prod_name}</td>
+                                <td>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(p.productDTO.prod_price)}</td>
+                                <td>{p.productDTO.prod_spec}</td>
+                                <td>{p.productDTO.prod_builder}</td>
+                                <td>{p.quantity}</td>
+                                <td>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(p.price)}</td>
+                            </tr>
+                        ))}
+                    </table>
                 </div>
                 <div className="selected-right">
 
